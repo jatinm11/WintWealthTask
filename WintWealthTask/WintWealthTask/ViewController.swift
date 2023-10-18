aca9e8cf174f5b34b1df0d31700b5108f5da3153
Jatin Menghani <jatinmenghani300@gmail.com>
2023-10-18 14:09:48 +0530

    Init Project

---
+++ WintWealthTask/WintWealthTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WintWealthTask
+//
+//  Created by Jatin Menghani on 18/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
