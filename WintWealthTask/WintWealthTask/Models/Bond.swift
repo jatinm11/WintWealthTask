7cb955abc8f72b548c8455907a1b0f7e3174aae1
Jatin Menghani <jatinmenghani300@gmail.com>
2023-10-18 14:14:20 +0530

    Added Bond Model

---
+++ WintWealthTask/WintWealthTask/Models/Bond.swift
@@ -0,0 +1,40 @@
+//
+//  Bond.swift
+//  WintWealthTask
+//
+//  Created by Jatin Menghani on 18/10/23.
+//
+
+import Foundation
+
+struct Bond: Codable {
+    
+    var isin: String
+    
+    var issuerName: String
+    
+    var issueMode: String
+    
+    var faceValue: Int
+    
+    var issueSize: String
+    
+    var timeLeftForMaturity: String?
+    
+    var maturityDate: String
+    
+    var couponRate: String
+    
+    var couponType: String?
+    
+    var interestRepayment: String?
+    
+    var creditRating: String
+    
+    var businessSector: String?
+    
+    var instrumentStatus: String
+    
+    var issuerNameSlug: String
+    
+}
